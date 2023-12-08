4a76f648ce7b7dd794b87ffee58783aba43adfce
Andreas Gottwald <andreas.gottwald@hotmail.de>
2023-12-08 11:58:12 +0100

    update submit button generally works

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+/* import logo from "./logo.svg"; */
+import "./App.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const [formData, setFormData] = useState({
+    name: "",
+    goodWeather: false,
+  });
+
+  /* function GetName() {
+
+  } */
+
+  /*   function GetGoodWeather() {}
+   */
+
+  function onAddActivity(event) {
+    event.preventDefault();
+    console.log("Activity added:", formData);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <h2>Add new activity</h2>
+      <label htmlFor="name">Name: </label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        value={formData.name}
+        onChange={(event) =>
+          setFormData({ ...formData, name: event.target.value })
+        }
+      ></input>
+      <br />
+      <br />
+
+      <label htmlFor="goodWeather">Good-weather activity: </label>
+
+      <input
+        type="checkbox"
+        id="goodWeather"
+        name="goodWeather"
+        checked={formData.goodWeather}
+        onChange={(event) =>
+          setFormData({ ...formData, goodWeather: event.target.checked })
+        }
+      ></input>
+      <br />
+      <br />
+      <button onClick={onAddActivity}>submit</button>
+    </form>
   );
 }
 
