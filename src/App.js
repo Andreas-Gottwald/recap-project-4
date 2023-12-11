b34c4be8b393c2b2b2e97427e863f1cbd59470e2
Andreas Gottwald <andreas.gottwald@hotmail.de>
2023-12-11 14:04:26 +0100

    add newEntry and onNewEntry ti app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import "./App.css";
-// import { useState } from "react";
+import { useState } from "react";
 //import { useEffect } from "react";
 // import useLocalStorageState from "use-local-storage-state";
 import Form from "./component/Form/Form.js";
 import List from "./component/List/List.js";
 
 export default function App() {
+  const [newEntryTrigger, setNewEntryTrigger] = useState(false);
+
+  const handleNewEntry = () => {
+    setNewEntryTrigger(true);
+  };
+
   return (
     <>
       <ul>
-        <List />
+        <List newEntryTrigger={newEntryTrigger} />
       </ul>
-      <Form />
-      {/* <Form onUpdateLocalStorageState={updateLocalStorageState} /> */}
+      <Form onNewEntry={handleNewEntry} />
     </>
   );
 }
